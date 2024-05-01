62e7a9b598bc4e8fbd1ac358d80ebaf6fcfd4515
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-05-02 01:30:12 +0600

    object and array destructuring added

---
+++ module1/src/1.8.ts
@@ -0,0 +1,17 @@
+{
+    // object destructuring
+    const me = {
+        id: 1520,
+        name: {
+            firstName: 'masud',
+            lastName: 'rahman'
+        },
+        contact: '01850556560',
+        isMarried: 'waiting'
+    }
+    const { id, name: { lastName: name }, contact: phone } = me; // we can get the lastName value by this method and we can alias the name also.
+
+    //array destructuring
+    const friends = ['monica', 'rachel', 'phoebe', 'chandler', 'joey', 'ross']
+    const [,,weirdOne,...rest] = friends; //here we used the rest operator for all the rest names and first two commas for first two elements and then we changed the name of phoebe to weirdOne.
+}
