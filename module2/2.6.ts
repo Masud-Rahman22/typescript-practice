13897dfa79fd38434c9f66d7e4e73192766acc0c
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-05-03 21:43:32 +0600

    constraints of tsc is added

---
+++ module2/2.6.ts
@@ -0,0 +1,21 @@
+{
+    // constraints 
+    const addCourseToStudent = <T extends {name: string; email: string}>(student: T)=>{
+        const course = 'web development course'
+        return {
+            ...student,
+            course
+        }
+    } // here when we extends the property that means without this property no object can call this function.
+
+    const student = addCourseToStudent({
+        name: 'masud',
+        email: 'masudrhmn6560@gmail.com',
+        address: 'narayanganj'
+    })
+    const student2 = addCourseToStudent({
+        name: 'masud',
+        // email: 'masudrhmn6560@gmail.com',
+        address: 'narayanganj'
+    }) // here without email there will be error.
+}
