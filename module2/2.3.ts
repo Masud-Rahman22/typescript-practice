a398946f9046fab1872c81fbfad9e4e6c62d2054
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-05-03 14:17:39 +0600

    generics for array of number , boolean ,string and also for tuple added

---
+++ module2/2.3.ts
@@ -0,0 +1,26 @@
+{
+    // generics
+    type GenericArray<T> = Array<T>; // generic array using dynamic value [T just a convention and it is referring params]
+
+    const arrayOfNumber : GenericArray<number> = [1,2,3,4]
+    const arrayOfString : GenericArray<string> = ['1','2','3','4']
+    const arrayOfBoolean : GenericArray<boolean> = [true, false, true]
+
+    // for array fo objects
+
+    const myself : GenericArray<{name: string, age: number}> = [
+        {
+            name: 'masud',
+            age: 25,
+        },
+        {
+            name: 'nitol',
+            age: 25,
+        },
+    ]
+
+    //  for tuples
+    type GenericTuple<x,y> = [x,y] 
+
+    const userWithId : GenericTuple<number, {name: string,age: number}>= [1234, {name: 'John', age: 19}]
+}
