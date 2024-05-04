c613156e4189030c0a70644bcbeb6a76c3309e96
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-05-04 15:42:50 +0600

    some important utility types are added including pick,omit,required,partial,readonly,record

---
+++ module2/2.11.ts
@@ -0,0 +1,54 @@
+{
+
+    // utility types
+    // pick type
+
+    type Myself = {
+        name: string;
+        age: number;
+        email?: string;
+        address: string;
+    }
+
+    type PickProperty = Pick<Myself, 'name' | 'age'>
+
+    // omit type means all property except that property
+
+    type OmitProperty = Omit<Myself, 'address'>
+
+    // Required type
+
+    type RequiredProperty = Required<Myself>
+
+    // partial type
+
+    type PartialProperty = Partial<Myself>
+
+    // ReadOnly type
+
+    type ReadOnlyProperty = Readonly<Myself>
+
+    const aboutMe: ReadOnlyProperty = {
+        name: 'rahman',
+        age: 24,
+        address: 'Dhaka'
+    }
+
+    aboutMe.age = 23; // can not alter readonly property
+
+    /*
+    properties of object are always string type and index of array is number
+    */
+
+    // record type
+
+    type MyObj = Record<string, string>
+
+    const emptyObj : Record<string, unknown> = {} // this is very important we will use it in our future projects.
+
+    const myObj : MyObj = {
+        a: 'a',
+        b: 'b',
+        c: 'c',
+    }
+}
