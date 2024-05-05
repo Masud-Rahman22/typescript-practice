89f292ad39e1ee015becfde4fd8165a780008f66
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-05-05 17:11:45 +0600

    acces modifiers of oop like private, protected, readonly

---
+++ module3/3.5.ts
@@ -0,0 +1,55 @@
+{
+    // access modifiers
+    // this is to understand private access modifier
+    // class BankAccount {
+    //     public readonly id: number; // by default public is added so no need to tell explicitly & readonly means can not change this property.
+    //     public name: string; // by default public is added so no need to tell explicitly
+    //     private _balance: number; // by default public is added so no need to tell explicitly & private means it iss only accessible inside the class and _balance this is just convention
+
+    //     constructor(id: number, name: string, balance: number){
+    //         this.id = id;
+    //         this.name = name;
+    //         this._balance = balance;
+    //     }
+
+    //     updateBalance(amount: number): number {
+    //         return this._balance = this._balance + amount;
+    //     }
+
+    //     getBalance():number{
+    //         return this._balance;
+    //     }
+
+    // }
+
+    // const myAccount = new BankAccount(123, 'my account', 0)
+    // myAccount.updateBalance(100000)
+    // const amount = myAccount.getBalance()
+    // console.log(amount)
+    
+    // this i sto understand protected access modifier
+    class BankAccount {
+        public readonly id: number; // by default public is added so no need to tell explicitly & readonly means can not change this property.
+        public name: string; // by default public is added so no need to tell explicitly
+        protected _balance: number; // by default public is added so no need to tell explicitly & protected means it is accessible inside the class as well as for the children of this class and _balance this is just convention
+
+        constructor(id: number, name: string, balance: number){
+            this.id = id;
+            this.name = name;
+            this._balance = balance;
+        }
+
+        updateBalance(amount: number): number {
+            return this._balance = this._balance + amount;
+        }
+
+        getBalance():number{
+            return this._balance;
+        }
+    }
+    class StudentAccount extends BankAccount{
+        test(){
+            this._balance // here _balance is accessible because this is child of BankAccounts
+        }
+    }
+}
